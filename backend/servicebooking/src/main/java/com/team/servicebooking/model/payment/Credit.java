ed914e43e785dd9969bb930177b58354b6ed6a73
Yamin Desktop <yamintayyar291@gmail.com>
2026-03-03 19:24:28 -0500

    Implement PaymentMethodStrategy class and subclasses

--- backend/servicebooking/src/main/java/com/team/servicebooking/model/payment/Credit.java
+++ backend/servicebooking/src/main/java/com/team/servicebooking/model/payment/Credit.java
@@ -1,8 +1,28 @@
 package servicebooking.src.main.java.com.team.servicebooking.model.payment;
 
-public class Credit implements PaymentMethodStrategy {
+import java.time.LocalDate;
+
+public class Credit extends PaymentMethodStrategy {
+
+    public Credit(String number, LocalDate expiry, String cvv ) {
+        this.number = number;
+        this.expiry = expiry;
+        this.number2 = cvv;
+    }
 
     public boolean validate() {
 
+        //validate number
+        if (!number.strip().matches("\\d{16}")) return false; //ensures string is  16 digits
+
+        //validate cvv
+        if (!number2.strip().matches("\\d{3}")) return false; //ensures string is  3 digits
+
+        //validate expiry
+        if (expiry.compareTo( LocalDate.now() ) < 0 ) return false; //ensures expiry date is after the current date
+
+        return true;
     }
+
+
 }
--- backend/servicebooking/src/main/java/com/team/servicebooking/model/payment/PaymentMethodStrategy.java
+++ backend/servicebooking/src/main/java/com/team/servicebooking/model/payment/PaymentMethodStrategy.java
@@ -14,4 +14,5 @@ public abstract class PaymentMethodStrategy {
     public boolean validate() {
         return false;
     }
+
 }
