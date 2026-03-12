890468a44e7006cb9f5dc42db4ac6f534f6841ea
EmmanuelIdeho <ohis98@gmail.com>
2026-03-11 23:10:06 -0400

    giving admin its own DTO layer

---
+++ backend/servicebooking/src/main/java/com/team/servicebooking/dto/AdminRequestDTO.java
@@ -0,0 +1,34 @@
+package com.team.servicebooking.dto;
+
+public class AdminRequestDTO {
+    private String name;
+    private String email;
+    private String password;
+
+    public AdminRequestDTO() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
